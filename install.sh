1af8fd73673d28976fd49c355cab7904a8038d39
Lachlan McCarty <lachm@users.noreply.github.com>
2014-12-14 01:52:46 -0500

    Fixed installer once more

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 URL="https://raw.githubusercontent.com/lachm/cmd-line-trash/master/"
 
 cd /usr/local/bin
-umask 755
 
 function download {
   wget "$URL$1"
+  chmod 755 "$1"
 }
 download empty-trash
 download list-trash
